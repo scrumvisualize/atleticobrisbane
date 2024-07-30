efec7c422cf43e1891ce786d61f46b77f7d230f8
Vinod Mathew <vinod.mathew@tes.com>
2024-07-31 00:09:48 +1000

    update insta, fb

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -55,10 +55,10 @@ const Footer = () => {
                 <h3 className="text-2xl text-[#e6e4f0] font-bold mt-6 mb-4">Follow Us</h3>
                 <div className="flex space-x-4 mt-4">
 
-                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white group">
+                    <a href="https://www.instagram.com/atleticobrisbanefc/?igsh=djZqZG8wc2d5ZjBl" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white group">
                         <img src="images/instagram.png" alt="Instagram" className="h-6 w-6 md:h-8 md:w-8 ml-2 rounded-full group-hover:animate-pulse" />
                     </a>
-                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white group">
+                    <a href="https://www.facebook.com/southsidesoccerstuds?mibextid=JRoKGi" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white group">
                         <img src="images/facebook.png" alt="Facebook" className="h-6 w-6 md:h-8 md:w-8 ml-2 rounded-full group-hover:animate-pulse border-2" />
                     </a>
                 </div>
