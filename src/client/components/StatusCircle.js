2f5ffab2b7f9c803dfe3b929e8aa4ec4e24c440e
Vinod Mathew <vinod.mathew@tes.com>
2024-07-31 19:15:39 +1000

    update rajesh image

--- src/client/components/StatusCircle.js
+++ src/client/components/StatusCircle.js
@@ -19,7 +19,7 @@ const StatsCircle = () => {
           <p className="text-sm md:text-base lg:text-lg text-[#ff66c4]">Active Players</p>
         </div>
         <div className="w-40 h-40 flex flex-col justify-center items-center bg-white bg-opacity-20 rounded-full p-6 md:p-8 lg:p-10 text-center shadow-lg">
-          <p className="text-xl md:text-2xl lg:text-2xl font-bold text-[#73d957]">48</p>
+          <p className="text-xl md:text-2xl lg:text-2xl font-bold text-[#73d957] animate-pulse">48</p>
           <p className="text-sm md:text-base lg:text-lg text-[#73d957]">Total Games</p>
         </div>
         <div className="w-40 h-40 flex flex-col justify-center items-center bg-white bg-opacity-20 rounded-full p-6 md:p-8 lg:p-10 text-center shadow-lg">
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
         'custom-center': 'center 5px', // Adjust the 10px value as needed
       },
     },
+    keyframes: {
+      pulse: {
+        '0%, 100%': { transform: 'scale(1.25)' },
+        '50%': { transform: 'scale(1.1)' },
+      },
+    },
+    animation: {
+      pulse: 'pulse 2s infinite',
+    },
   },
   plugins: [],
 }
